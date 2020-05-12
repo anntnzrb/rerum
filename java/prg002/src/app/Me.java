306e360425f2d92c4717b2ab4fd0ae6c8c1d9b3b
anntnzrb <anntnzrb@hotmail.com>
2020-05-12 13:27:52 -0500

    organization

---
+++ java/prg002/src/app/Me.java
@@ -0,0 +1,15 @@
+package app;
+
+/* notar que no es posible correr esta clase ya que no hay metodo main() */
+
+public class Me {
+    public static void
+    sayName() {
+        System.out.println("annt");
+    }
+
+    public static void
+    sayAge() {
+        System.out.println(20);
+    }
+}
