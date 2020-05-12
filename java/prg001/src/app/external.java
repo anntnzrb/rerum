005d9531925ec3bb374bdb5c2e5188c4efd74e48
anntnzrb <anntnzrb@hotmail.com>
2020-05-12 13:27:52 -0500

    test

---
+++ java/prg001/src/app/external.java
@@ -0,0 +1,14 @@
+package app;
+
+public class external {
+    public static void
+    main(String[] args) {
+        System.out.println("he-ya desde otra clase");
+        salute();
+    }
+    public static void
+    salute() {
+        /* esto se ejecutara unicamente si es llamado en main() */
+        System.out.println("he-ya desde un metodo diferente de main()");
+    }
+}
