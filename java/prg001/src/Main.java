3bab7266547bfd8e19ae5fe05eb9393b618b61bc
anntnzrb <anntnzrb@hotmail.com>
2020-05-17 15:11:20 -0500

    ++

--- java/prg001/src/Main.java
+++ java/prg001/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void
     main(String[] args) {
-        /* se llama al metodo salute() de la clase external
+        /* se llama al metodo salute() de la clase External
          * notar que salute() es un metodo publico
          */
         External.salute();
